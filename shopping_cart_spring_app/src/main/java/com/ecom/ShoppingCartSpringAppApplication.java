330e44816f78f4d5e3e0a2ca6e0aff050b1ed3a7
MoussaKhalilMoussa <moussa.khalil.92@gmail.com>
2024-12-02 14:09:36 +0300

    Front end implementation v-4

---
+++ shopping_cart_spring_app/src/main/java/com/ecom/ShoppingCartSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartSpringAppApplication.class, args);
+	}
+
+}
